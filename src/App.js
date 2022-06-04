2e96339d4b4e3630b32a78f69627d0486b463cbd
utsavatdeep <utsavdeep0612@gmail.com>
2022-06-04 18:58:07 +0530

    this commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello w</h1>
+      <div className="jumbotron"></div>
+    </div>
+  );
+}
+
+export default App;
